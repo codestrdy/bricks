0922c0f09f55add253132eb6f3b2bc86050d5f90
fahmi <fahmiismail.dev@gmail.com>
2025-08-31 00:35:10 +0700

    add new bricks : routers

---
+++ bricks/routers/__brick__/{{name.snakeCase()}}_routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+@TypedGoRoute<{{name.pascalCase()}}Route>(path: '/{{name.lowerCase()}}')
+class {{name.pascalCase()}}Route extends GoRouteData with ${{name.pascalCase()}}Route {
+  {{name.pascalCase()}}Route();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return Container();
+  }
+}
