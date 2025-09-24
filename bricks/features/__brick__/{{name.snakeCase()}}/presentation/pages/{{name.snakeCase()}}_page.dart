376d4c49d8079244656aaca0a5185ea172837424
fahmi <fahmiismail.dev@gmail.com>
2025-09-24 20:26:22 +0700

    fix part name bug

--- bricks/features/__brick__/{{name.snakeCase()}}/presentation/pages/{{name.snakeCase()}}_page.dart
+++ bricks/features/__brick__/{{name.snakeCase()}}/presentation/pages/{{name.snakeCase()}}_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class {{name.pascalCase()}} extends ConsumerWidget {
-  const {{name.pascalCase()}}({super.key});
+class {{name.pascalCase()}}Page extends ConsumerWidget {
+  const {{name.pascalCase()}}Page({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
